9de4b9361cb8376b15e769d24c0a4f7b40d13827
Francisco Souza <francisco@franciscosouza.net>
2010-10-27 23:12:16 -0200

    Removendo espaços em branco

--- hello_express.js
+++ hello_express.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express.createServer();
 
 app.get('/', function (request, response) {
-    response.send('Hello world!');        
+    response.send('Hello world!');
 });
 
 app.listen(3000);
